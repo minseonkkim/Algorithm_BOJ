8d47359131ded53ba45fe491800a4cf7771f6576
Minseon Kim <minsun9856@gmail.com>
2021-10-24 18:05:46 +0900

    Create 2231.cpp

---
+++ Brute force/2231.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+	
+	for(int i = 1; i < num; i++){
+		sum = i;
+		part = i;
+		
+		while(part){
+			sum += part % 10;
+			part /= 10;
+		}
+			
+		if(num == sum){
+			cout << i << endl;
+			return 0;
+		}
+
+	}
+	cout << "0" << endl;
+}

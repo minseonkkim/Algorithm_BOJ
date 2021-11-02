f0c57dacf10f57e5f97763875726c32913ad4ff2
Minseon Kim <minsun9856@gmail.com>
2021-11-02 15:34:36 +0900

    Create 13699.cpp

---
+++ Dynamic programming/13699.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long arr[36] = { 0, };
+	int n;
+	cin >> n;
+	
+	arr[0] = 1;
+	arr[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j < i; j++) {
+			arr[i] += arr[j] * arr[i - j - 1];
+		}
+	}
+
+	cout << arr[n] << endl;
+
+	return 0;
+}

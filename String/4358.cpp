e9d38083adda08a061fb63c081efee08499da337
Minseon Kim <minsun9856@gmail.com>
2022-02-04 16:57:53 +0900

    생태학

---
+++ String/4358.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+	string str;
+	map<string, float> m;
+	float cnt = 0;
+	while(getline(cin, str)){
+		cnt++;
+		if(m.find(str) == m.end()){
+			m[str] = 1;
+		} else{
+			m[str]++;
+		}
+	}
+	
+	cout << fixed;
+	cout.precision(4);
+	for(auto iter = m.begin(); iter != m.end(); iter++){
+		float x = (iter->second / cnt) * 100;
+		cout << iter->first << " " << x << endl;
+		
+	}
+	return 0;
+}

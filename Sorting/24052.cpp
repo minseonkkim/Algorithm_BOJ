ad282208eff3f3190eaa07393a2d42c24bb5ea4c
Minseon Kim <minsun9856@gmail.com>
2022-02-16 11:11:05 +0900

    알고리즘 수업 - 삽입 정렬 2

---
+++ Sorting/24052.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a[10001];
+	int n, k, loc, newItem, cnt = 0;
+	cin >> n >> k;
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	
+	for(int i = 1; i < n; i++){
+		loc = i - 1;
+		newItem = a[i];
+		
+		while(0 <= loc && newItem < a[loc]){
+			a[loc + 1] = a[loc];
+			loc--;
+			cnt++;
+			if(cnt == k){
+				for(int p = 0; p < n; p++){
+					cout << a[p] << " ";
+				}
+				return 0;
+			}
+		}
+		if(loc + 1 != i) {
+			a[loc + 1] = newItem;
+			cnt++;
+			if(cnt == k){
+				for(int p = 0; p < n; p++){
+					cout << a[p] << " ";
+				}
+				return 0;
+			}
+		}
+	}
+	cout << -1 << endl;
+	return 0;
+}

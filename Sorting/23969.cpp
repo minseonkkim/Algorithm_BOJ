881187623dd5f04efb28bf09ff120c953e02ac42
Minseon Kim <minsun9856@gmail.com>
2022-02-16 10:21:09 +0900

    알고리즘 수업 - 버블 정렬 2

---
+++ Sorting/23969.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, k, cnt = 0;
+	int v[10001];
+	
+	cin >> n >> k;
+	for(int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	for(int i = n - 1; i > 0; i--){
+		for(int j = 0; j < i; j++){
+			if(v[j] > v[j + 1]){
+				int t = v[j];
+				v[j] = v[j + 1];
+				v[j + 1] = t;
+				cnt++;
+				if(cnt == k){
+					for(int p = 0; p < n; p++){
+						cout << v[p] << " ";
+					}
+					return 0;
+				}
+			}
+		}
+	}
+	cout << -1 << endl;
+	return 0;
+}

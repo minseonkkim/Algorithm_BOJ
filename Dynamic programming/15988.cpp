93ea133672cb234216e82d10ddcb47a6242ff86d
Minseon Kim <minsun9856@gmail.com>
2022-02-25 13:48:27 +0900

    1, 2, 3 더하기 3

--- Dynamic programming/15988.cpp
+++ Dynamic programming/15988.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
 	int n, num;
 	long long dp[1000001] = {0, 1, 2, 4};
-	for(int j = 4; j <= 1000001; j++){
-		dp[j] = ((dp[j - 1] + dp[j - 2])% 1000000009 + dp[j - 3]) % 1000000009;
+	for(int i = 4; i <= 1000001; i++){
+		dp[i] = ((dp[i - 1] + dp[i - 2])% 1000000009 + dp[i - 3]) % 1000000009;
 	}
 	
     cin >> n;

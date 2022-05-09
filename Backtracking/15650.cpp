6610648e98b4fd90431f39360ae47676f038a4c2
Minseon Kim <minsun9856@gmail.com>
2022-05-09 23:31:27 +0900

    n과 m (2)

---
+++ Backtracking/15650.cpp
@@ -0,0 +1,30 @@
+include <iostream>
+using namespace std;
+
+int n, m;
+int arr[9];
+bool isused[9];
+
+void func(int s, int k) {
+	if (k == m + 1) {
+		for (int i = 1; i <= m; i++)
+			cout << arr[i] << " ";
+		cout << "\n";
+	}
+	else {
+		for (int i = s; i <= n; i++) {
+			if (!isused[i]) {
+				isused[i] = true;
+				arr[k] = i;
+				func(i + 1, k + 1);
+				isused[i] = false;
+			}
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	func(1, 1);
+	return 0;
+}

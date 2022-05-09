28c6bc26716b4193b4b3e7874af2db456f0c8360
Minseon Kim <minsun9856@gmail.com>
2022-05-10 08:48:35 +0900

    N과 M (8)

---
+++ Backtracking/15657.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> v;
+int arr[9];
+
+void func(int s, int k){
+	if(k == m + 1){
+		for(int i = 1; i <= m; i++)
+			cout << arr[i] << " ";
+		cout << "\n";
+	} 
+	else{
+		for(int i = s; i <= n; i++){
+			arr[k] = v[i];
+			func(i, k + 1);
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	int x;
+	v.push_back(0);
+	for(int i = 1; i <= n; i++) {
+		cin >> x;
+		v.push_back(x);
+	}
+	sort(v.begin(), v.end());
+	func(1, 1);
+	return 0;
+}

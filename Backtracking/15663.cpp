464a69a51d2f7c08da8410ee96b3eae43df928c4
Minseon Kim <minsun9856@gmail.com>
2022-05-10 13:03:45 +0900

    N과 M (9)

---
+++ Backtracking/15663.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> v;
+int arr[9];
+bool isused[9];
+
+void func(int k){
+	int last = 0;
+	if(k == m + 1){
+		for(int i = 1; i <= m; i++)
+			cout << arr[i] << " ";
+		cout << "\n";
+	} 
+	else{
+		for(int i = 1; i <= n; i++){
+			if(!isused[i] && v[i] != last){
+				isused[i] = true;
+				arr[k] = v[i];
+				last = arr[k];
+				func(k + 1);
+				isused[i] = false;
+			}
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	int x;
+	v.push_back(0);
+	for(int i = 1; i <= n; i++) {
+		cin >> x;
+		v.push_back(x);
+	}
+	sort(v.begin(), v.end());
+	func(1);
+	return 0;
+}

25e094c398e9c11773ecd9c0526b45a1d9073d8d
Minseon Kim <minsun9856@gmail.com>
2022-05-16 13:24:33 +0900

    가장 긴 증가하는 부분 수열

---
+++ Dynamic programming/11053.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> arr(n, 0);
+	vector<int> dp(n, 1);
+    
+	for (int x = 0; x < n; x++) {
+		cin >> arr[x];
+	}
+	int answer = 0;
+	for (int y = 0; y < n; y++) {
+		for (int x = 0; x < y; x++) {
+			if (arr[x] < arr[y]) {
+				dp[y] = max(dp[y], dp[x] + 1);
+			}
+		}
+		answer = max(answer, dp[y]);
+	}
+	cout << answer;
+}
